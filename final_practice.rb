487ca62b16b74b7ae4696b91f1ebf53ea630b1f3
andi lovetto <andilovetto@gmail.com>
2023-04-01 17:59:18 -0600

    Add final practice answers

--- final_practice.rb
+++ final_practice.rb
@@ -2,7 +2,7 @@
 # For each exercise below, write the method according to the requirements. 
 # Call each method at least twice and store the return value in a variable. 
 # Use the puts or print command to see the return value in the console.
-
+require 'pry'
 
 # 1: Write a method named greeting that returns a string with a general greeting. 
 def greeting
@@ -14,47 +14,54 @@ puts hi
 # What is the return value of your method?
 # How many arguments did you pass your method?
 
-The return value of the greeting method is: "Hi there! How are you"
-I did not pass any arguments, because the greeting was general.
+#The return value of the greeting method is: "Hi there! How are you"
+#I did not pass any arguments, because the greeting was general.
 
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
 def custom_greeting(name)
     return "Hi there, #{name}!"
-puts custom_greeting("Andi")
 end
+custom_greeting("Andi")
 
 # What is the return value of your method?
 # How many arguments did you pass your method?
 # What data type was your argument(s)?
 
-The return value of the custom_greeting method 
+# The return value of the custom_greeting method is "Hi there, Andi!"
+# I passed 1 argument to my method. 
+# The data type of my argument is a string.
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
+first_name = "andi" 
+middle_name = "ray" 
+last_name = "lovetto"
+
+def greet_person(first, middle, last)
+    "Hi #{first} #{middle} #{last}, how are you today?"
+
+end
+puts greet_person(first_name, middle_name, last_name)
 
 # What is the return value of your method?
 # How many arguments did you pass your method?
 # What data type was your argument(s)?
 
+# the return value of my greet_person method is "hi andi ray lovetto, how are you today?"
+# I passed 3 arguments in my method. The data type of those arguments were strings.
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
 # Bonus: Print a sentence that interpolates the return value of your square method.
+def square(number)
+  number * number
+
+end
+
+p square(4)
+
 
 # What is the return value of your method?
 # How many arguments did you pass your method?
 # What data type was your argument(s)?
 
-
-# 5: Write a method named check_stock that satisfies the following interaction pattern:
-# Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
-
-check_stock(4, "Coffee");
-# => "Coffee is stocked"
-
-check_stock(3, "Tortillas");
-# => "Tortillas - running LOW"
-
-check_stock(0, "Cheese");
-# => "Cheese - OUT of stock!"
-
-check_stock(1, "Salsa");
-# => "Salsa - running LOW"
+# the return value of the square method is 16; I passed 1 argument into the method. 
+# the date type of the argument provided was an integer 
--- self_eval.md
+++ self_eval.md
@@ -1,4 +1,5 @@
 # Self-Evaluation
 
 - Reflect on your learning and work today. What are you most proud of?
+
 - What is the power of using methods in our applications?
